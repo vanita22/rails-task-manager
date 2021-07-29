bc6b337bd60ec406b1d90ca7681853c52f52820c
vanessa perez <inesvanessapcm@gmail.com>
2021-07-29 16:19:34 -0400

    agregando vistas y las acciones en el controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
+  #create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  #update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  #delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
